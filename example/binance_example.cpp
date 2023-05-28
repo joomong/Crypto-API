3740ea9232aa84d56432e23409fb869afdba1147
joomong <jmgo97@gmail.com>
2023-05-28 17:33:24 +0900

    -add .gitignore

--- example/binance_example.cpp
+++ example/binance_example.cpp
@@ -1,14 +1,14 @@
 #include <string>
 #include <future>
-#include "../src/binance.h"
+#include <thread>
 #include "../src/binanceusdm.h"
 
 int on_message(Json::Value &result_json){
     std::cout << result_json << std::endl;
 };
 
-std::string api_key = "api_key";
-std::string api_secret = "api_secret";
+std::string api_key = "rqUkbaF9xvMrRJQs9m87g2GlgxMKkaHQUiMHFInTTBoJxzFG8hM9tdXpSN0fj0PY";
+std::string api_secret = "JSy54Uaw62k898J17mRPmC8ptL2VBdBTTORr4x3pOflH6dFDa0NeIFw279Pzyvex";
 
 int main(){
     Json::Value result_json;
@@ -19,7 +19,21 @@ int main(){
     std::string symbol("BTCUSDT");
     std::string interval("1m");
 
-    Binanceusdm::GetKlines(symbol, interval, 10, result_json);
+//    Binanceusdm::connect_endpoint(on_message,
+//                                  BINANCE_WS_HOST,
+//                                  BINANCE_WS_PORT,
+//                                  "/ws/btcusdt@aggTrade");
+//    Binanceusdm::enter_event_loop();
+
+    Binanceusdm::PostLimitSell(symbol, 0.001, 50000, result_json);
+    std::cout << result_json << std::endl;
+
+//    std::string order_id = result_json["orderId"].asString();
+
+    Binanceusdm::CancelAllOrders(symbol,result_json);
+    std::cout << result_json << std::endl;
+
+    Binanceusdm::PostLimitSell(symbol, 0.001, 50000, result_json);
     std::cout << result_json << std::endl;
 
     return 0;
